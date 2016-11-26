2b915e42b44bc29eaa6225f373497a13fd14285b
Oleg Obleukhov <oleg.obleukhov@innogames.com>
2016-11-26 13:37:25 +0100

    Added pagefailt detector in mem.sh

--- mem.sh
+++ mem.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
 # This is bar symbol
 bar="\033[7m \033[0m"
-
+pgmajfault=$(grep pgmajfault /proc/vmstat | awk '{print $2}')
 while true
 do
     active_pages=$(grep nr_active_file /proc/vmstat | awk '{print $2}')
     inactive_pages=$(grep nr_inactive_file /proc/vmstat | awk '{print $2}')
+    pgmajfault_new=$(grep pgmajfault /proc/vmstat | awk '{print $2}')
     total=$(echo $(grep ^MemTotal /proc/meminfo | awk '{print $2}')/4 | bc)
-    
+
     # Percentage we need to display within 1 terminal
     active_percentage=$(echo $active_pages*100/$total | bc)
     inactive_percentage=$(echo $inactive_pages*100/$total | bc)
-    
+
     echo "ACTIVE: $(echo ${active_percentage} | bc)%"
     for ((i=0; i<=active_percentage; i++))
     do
@@ -21,10 +22,16 @@ do
     echo
     echo "INACTIVE: $(echo ${inactive_percentage} | bc)%"
     for ((i=0; i<=inactive_percentage; i++))
-        do
-                echo -ne "\033[7m \033[0m"
-        done
-    
+    do
+        echo -ne "\033[7m \033[0m"
+    done
+
+    if [ "$pgmajfault_new" -gt "$pgmajfault" ]; then
+        pgmajfault=$pgmajfault_new
+        echo
+        echo "PAGEFAULT!"
+    fi
+
     sleep 1
     clear
 done
